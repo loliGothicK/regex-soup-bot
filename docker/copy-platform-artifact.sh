d52569c8ee13c7c90af6536eb568b913623082f1
Kory <korygm33@gmail.com>
2021-09-19 05:05:29 +0900

    fix: correctly copy downloaded artifact to /usr/local/bin

--- docker/copy-platform-artifact.sh
+++ docker/copy-platform-artifact.sh
@@ -2,10 +2,10 @@
 
 case $(uname -m) in
   # linux/amd64
-  "x86_64"  ) cp /work/artifacts/x86_64-unknown-linux-gnu      /usr/local/bin/regexsoup ;;
+  "x86_64"  ) cp /work/artifacts/x86_64-unknown-linux-gnu/regexsoup      /usr/local/bin/regexsoup ;;
   # linux/arm/v7
-  "armv7l"  ) cp /work/artifacts/armv7-unknown-linux-gnueabihf /usr/local/bin/regexsoup ;;
+  "armv7l"  ) cp /work/artifacts/armv7-unknown-linux-gnueabihf/regexsoup /usr/local/bin/regexsoup ;;
   # linux/arm64/v8
-  "aarch64" ) cp /work/artifacts/aarch64-unknown-linux-gnu     /usr/local/bin/regexsoup ;;
+  "aarch64" ) cp /work/artifacts/aarch64-unknown-linux-gnu/regexsoup     /usr/local/bin/regexsoup ;;
   * ) exit 1 # we do not expect other platform
 esac
